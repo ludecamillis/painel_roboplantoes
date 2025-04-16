77a121d22b96ac2bf625ec49777be1ba33dfbf81
ludecamillis <36630661+ludecamillis@users.noreply.github.com>
2025-04-16 11:59:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
